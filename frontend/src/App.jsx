d147ab5d7e60875a6423114d376d0e7ddb3ae774
mahmud-alam <official.mahmud23@gmail.com>
2025-03-17 13:05:30 +0600

    create routes for all pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,29 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Doctors from "./pages/Doctors";
+import Login from "./pages/Login";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Profile from "./pages/Profile";
+import Appointments from "./pages/Appointments";
+import Appointment from "./pages/Appointment";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="mx-4 sm:mx-[10%]">
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/doctors" element={<Doctors/>}/>
+        <Route path="/doctors/:speciality" element={<Doctors/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+        <Route path="/appointments" element={<Appointments/>}/>
+        <Route path="/appointment/:docId" element={<Appointment/>}/>
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
