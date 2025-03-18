c91a354c03d879a01d9b375f9929d2150a8122af
mahmud-alam <official.mahmud23@gmail.com>
2025-03-18 12:54:05 +0600

    create dynamic nam menus

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,29 +1,29 @@
-import { NavLink } from 'react-router-dom';
-import {assets} from '../assets/assets_frontend/assets'
+import { NavLink } from "react-router-dom";
+import { assets } from "../assets/assets_frontend/assets";
+
+const navLinks = [
+  { title: "Home", link: "/" },
+  { title: "All Doctors", link: "/doctors" },
+  { title: "About", link: "/about" },
+  { title: "Contact", link: "/contact" },
+];
+
 const Navbar = () => {
   return (
-    <div>
-      <img src={assets.logo} alt="" />
-      <ul>
-        <NavLink>
-            <li>HOME</li>
-            <hr />
-        </NavLink>
-        <NavLink>
-            <li>ALL DOCTORS</li>
-            <hr />
-        </NavLink>
-        <NavLink>
-            <li>ABOUT</li>
-            <hr />
-        </NavLink>
-        <NavLink>
-            <li>CONTACT</li>
-            <hr />
-        </NavLink>
+    <div className="flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400">
+      <img className="w-44 cursor-pointer" src={assets.logo} alt="" />
+
+      <ul className="hidden md:flex items-start gap-5 font-medium uppercase">
+        {navLinks.map((nav, index) => (
+          <NavLink key={index} to={nav.link}>
+            <li className="py-1">{nav.title}</li>
+            <hr className="hidden border-none outline-none h-0.5 bg-primary w-3/5 m-auto" />
+          </NavLink>
+        ))}
       </ul>
-      <div>
-        <button>Create Account</button>
+
+      <div className="flex items-center gap-4">
+        <button className="bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block">Create Account</button>
       </div>
     </div>
   );
