ccddc86e42efaa91ed2c5b45338f0f6842b7195c
mahmud-alam <official.mahmud23@gmail.com>
2025-03-18 13:02:45 +0600

    add useNavigate to navigate to login page

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import { assets } from "../assets/assets_frontend/assets";
 
 const navLinks = [
@@ -9,6 +9,8 @@ const navLinks = [
 ];
 
 const Navbar = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400">
       <img className="w-44 cursor-pointer" src={assets.logo} alt="" />
@@ -23,7 +25,9 @@ const Navbar = () => {
       </ul>
 
       <div className="flex items-center gap-4">
-        <button className="bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block">Create Account</button>
+        <button onClick={()=>navigate('/login')} className="bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block">
+          Create Account
+        </button>
       </div>
     </div>
   );
