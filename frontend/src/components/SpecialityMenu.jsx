d84a8920c97a13661af25e71b6ac1f36ef8cd296
mahmud-alam <official.mahmud23@gmail.com>
2025-03-19 15:31:51 +0600

    create the structure of the speciality menu component

--- frontend/src/components/SpecialityMenu.jsx
+++ frontend/src/components/SpecialityMenu.jsx
@@ -1,6 +1,20 @@
+import { Link } from "react-router-dom"
+import { specialityData } from "../assets/assets_frontend/assets"
+
 const SpecialityMenu = () => {
   return (
-    <div>SpecialityMenu</div>
+    <div id="speciality">
+        <h1>Find by Speciality</h1>
+        <p>Simply browse through our extensive list of trusted doctors, schedule your appointment hassle-free.</p>
+        <div>
+            {specialityData.map((item, index) => (
+                <Link key={index} to={`/doctors/${item.speciality}`}>
+                    <img src={item.image} alt="" />
+                    <p>{item.speciality}</p>
+                </Link>
+            ))}
+        </div>
+    </div>
   )
 }
 
