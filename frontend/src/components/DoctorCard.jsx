02b98dce527dcbb90e7cd8e52dca64367cb193b8
mahmud-alam <official.mahmud23@gmail.com>
2025-03-23 09:42:02 +0600

    split TopDoctors section and DoctorCard section

---
+++ frontend/src/components/DoctorCard.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const DoctorCard = ({ doctor }) => {
+  const navigate = useNavigate();
+
+  return (
+    <div
+      onClick={() => navigate(`/appointment/${doctor._id}`)}
+      className="border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500"
+    >
+      <img className="bg-blue-50" src={doctor.image} alt={doctor.name} />
+      <div className="p-4">
+        <div className="flex items-center gap-2 text-sm text-center text-green-500">
+          <p className="w-2 h-2 bg-green-500 rounded-full"></p>
+          <p>Available</p>
+        </div>
+        <p className="text-gray-900 text-lg font-medium">{doctor.name}</p>
+        <p className="text-gray-600 text-sm">{doctor.speciality}</p>
+      </div>
+    </div>
+  );
+};
+
+export default DoctorCard;
