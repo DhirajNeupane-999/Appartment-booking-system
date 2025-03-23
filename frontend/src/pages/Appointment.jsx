faf9258042d685ce6a819e59bae250c3648acb31
mahmud-alam <official.mahmud23@gmail.com>
2025-03-23 15:27:10 +0600

    create appointment page structure

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -1,4 +1,22 @@
+import { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom"
+import { AppContext } from "../context/AppContext";
+
 const Appointment = () => {
+  const { docId } = useParams();
+  const { doctors } = useContext(AppContext)
+
+  const [docInfo, setDocInfo] = useState(null);
+
+  const fetchDoc = async () => {
+    const docInfo = doctors.find(doc => doc._id === docId)
+    setDocInfo(docInfo);
+  }
+
+  useEffect(()=>{
+    fetchDoc();
+  },[doctors,docId])
+
   return (
     <div>Appointment</div>
   )
