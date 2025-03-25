9aa0a3853e12e332b7419a96bb322d9caa8d3d3c
mahmud-alam <official.mahmud23@gmail.com>
2025-03-25 10:21:23 +0600

    Book an appointment button

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -151,6 +151,7 @@ const Appointment = () => {
                 <p onClick={()=>setSlotTime(item.time)} className={`text-sm font-light flex-shrink-0 px-5 py-2 rounded-full cursor-pointer ${item.time === slotTime ? "bg-primary text-white" : "text-gray-400 border border-gray-300"}`} key={index}>{item.time.toLowerCase()}</p>
               ))}
           </div>
+          <button className="bg-primary text-white text-sm font-light px-14 py-3 rounded-full my-6 cursor-pointer">Book an appointment</button>
         </div>
       </div>
     )
