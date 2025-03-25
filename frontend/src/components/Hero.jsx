6d5acb4dd0a6f9aafe6a256775cf99e11de85682
mahmud-alam <official.mahmud23@gmail.com>
2025-03-25 12:30:06 +0600

    fix format of hero section

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import { assets } from "../assets/assets_frontend/assets";
 
 const Hero = () => {
@@ -13,17 +12,30 @@ const Hero = () => {
           <img className="w-28" src={assets.group_profiles} alt="" />
           <p>
             Simply browse through our extensive list of trusted doctors,
-            <br className="hidden sm:block"/> schedule your appointment hassle-free.
+            <br className="hidden sm:block" /> schedule your appointment
+            hassle-free.
           </p>
         </div>
-        <a href="#speciality" className="group flex items-center gap-2 bg-white px-8 py-3 rounded-full text-gray-600 text-sm m-auto md:m-0 hover:scale-105 transition-all duration-300">
-          Book Appointment <img className="w-3 transition-transform duration-300 group-hover:translate-x-2" src={assets.arrow_icon} alt="" />
+        <a
+          href="#speciality"
+          className="group flex items-center gap-2 bg-white px-8 py-3 rounded-full text-gray-600 text-sm m-auto md:m-0 hover:scale-105 transition-all duration-300"
+        >
+          Book Appointment
+          <img
+            className="w-3 transition-transform duration-300 group-hover:translate-x-2"
+            src={assets.arrow_icon}
+            alt=""
+          />
         </a>
       </div>
 
       {/* ---------- right side ---------- */}
       <div className="md:w-1/2 relative">
-        <img className="w-full md:absolute bottom-0 h-auto rounded-lg" src={assets.header_img} alt="" />
+        <img
+          className="w-full md:absolute bottom-0 h-auto rounded-lg"
+          src={assets.header_img}
+          alt=""
+        />
       </div>
     </div>
   );
