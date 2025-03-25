1775c7e6f048dd6edf95671eeea1716bd0b24c6f
mahmud-alam <official.mahmud23@gmail.com>
2025-03-25 14:30:29 +0600

    create about page structure

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,7 +1,30 @@
+import { assets } from "../assets/assets_frontend/assets";
+
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div>
+      <div>
+        <p>
+          ABOUT <span>US</span>
+        </p>
+      </div>
 
-export default About
+      <div>
+        <img src={assets.about_image} alt="" />
+        <div>
+          <p>
+            Welcome to Prescripto, your trusted partner in managing your
+            healthcare needs conveniently and efficiently. At Prescripto, we
+            understand the challenges individuals face when it comes to
+            sheduling doctor appointments and managing their health records.
+          </p>
+          <p></p>
+          <b>Our Vision</b>
+          <p></p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
