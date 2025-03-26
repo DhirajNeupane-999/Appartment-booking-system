40ce303828224965bb8ce9716f988af6e35a53b2
Mahmud-Alam <official.mahmud23@gmail.com>
2025-03-26 21:38:40 +0600

    add para into about page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -16,11 +16,23 @@ const About = () => {
             Welcome to Prescripto, your trusted partner in managing your
             healthcare needs conveniently and efficiently. At Prescripto, we
             understand the challenges individuals face when it comes to
-            sheduling doctor appointments and managing their health records.
+            scheduling doctor appointments and managing their health records.
           </p>
-          <p></p>
+          <p>
+            Prescripto Is Committed To Excellence In Healthcare Technology. We
+            Continuously Strive To Enhance Our Platform, Integrating The Latest
+            Advancements To Improve User Experience And Deliver Superior
+            Service. Whether You're booking Your First Appointment Or Managing
+            ongoing care, Prescripto is Here To Support You Every Step Of The
+            Way
+          </p>
           <b>Our Vision</b>
-          <p></p>
+          <p>
+            Our Vision At Prescripto Is to A seamless Healthcare Experience for
+            Every User. We Aim To Bridge The Gap between Patients And Healthcare
+            providers, making it easier for fou to access The Care You Need,
+            When you need it.
+          </p>
         </div>
       </div>
     </div>
