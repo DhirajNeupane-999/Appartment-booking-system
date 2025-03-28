1600fa000a39d085298a65608f9366c40aa9a6cb
Mahmud-Alam <official.mahmud23@gmail.com>
2025-03-28 11:22:36 +0600

    create contact page structure

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,7 +1,27 @@
+import { assets } from "../assets/assets_frontend/assets";
+
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <div>
+      <div className="text-center text-2xl pt-10 text-gray-500 uppercase">
+        <p>
+          Contact <span className="text-gray-700 font-medium">Us</span>
+        </p>
+      </div>
 
-export default Contact
+      <div>
+        <img src={assets.contact_image} alt="" />
+        <div>
+          <p>Our Office</p>
+          <p>Mirpur 10, Dhaka<br />Bangladesh</p>
+          <p>Email: mahmudalam.official@gmail.com</p>
+          <p>Careers at Prescripto</p>
+          <p>Learn more about our teams and job openings.</p>
+          <button>Explore Jobs</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
