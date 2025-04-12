d2ea815201c649e46ef6a0cf5dc47574512ce73d
Mahmud-Alam <official.mahmud23@gmail.com>
2025-04-12 23:23:48 +0600

    login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -19,27 +19,27 @@ const Login = () => {
 
   return (
     <form className="min-h-[80vh] flex items-center">
-      <div className="flex flex-col gap-3 w-full max-w-[400px] mx-auto p-4 border border-gray-300 rounded-lg shadow-md">
-        <p>{state === "Sign Up" ? "Create Account" : "Login"}</p>
+      <div className="flex flex-col gap-3 m-auto items-start p-8 min-w-[340] sm:min-w-96 border rounded-xl text-zinc-600 text-sm shadow-lg">
+        <p className="text-2xl font-semibold">{state === "Sign Up" ? "Create Account" : "Login"}</p>
         <p>
           Please {state === "Sign Up" ? "sign up" : "log in"} to book
           appointment!
         </p>
-        <div>
+        <div className="w-full">
           <p>Full Name</p>
-          <input type="text" onChange={(e) => setName(e.target.value)} value={name} required/>
+          <input className="border border-zinc-300 rounded w-full p-2 mt-1" type="text" onChange={(e) => setName(e.target.value)} value={name} required/>
         </div>
 
-        <div>
+        <div className="w-full">
           <p>Email</p>
-          <input type="email" onChange={(e) => setEmail(e.target.value)} value={email} required/>
+          <input className="border border-zinc-300 rounded w-full p-2 mt-1" type="email" onChange={(e) => setEmail(e.target.value)} value={email} required/>
         </div>
 
-        <div>
+        <div className="w-full">
           <p>Password</p>
-          <input type="password" onChange={(e) => setPassword(e.target.value)} value={password} required/>
+          <input className="border border-zinc-300 rounded w-full p-2 mt-1" type="password" onChange={(e) => setPassword(e.target.value)} value={password} required/>
         </div>
-        <button>{state === "Sign Up" ? "Create Account" : "Login"}</button>
+        <button className="bg-primary text-white w-full py-2 rounded-md text-base">{state === "Sign Up" ? "Create Account" : "Login"}</button>
       </div>
     </form>
   );
