958790b826bd24209a9c084c408174c3c01aaf56
Mahmud-Alam <official.mahmud23@gmail.com>
2025-04-28 23:08:10 +0600

    structure of appointments page

--- frontend/src/pages/Appointments.jsx
+++ frontend/src/pages/Appointments.jsx
@@ -1,7 +1,37 @@
-const Appointments = () => {
-  return (
-    <div>Appointments</div>
-  )
-}
+import { useContext } from "react";
+import { AppContext } from "../context/AppContext";
 
-export default Appointments
+const Appointments = () => {
+  const { doctors } = useContext(AppContext);
+  return (
+    <div>
+      <p>My Appointments</p>
+      <div>
+        {doctors.slice(0, 2).map((doc, index) => (
+          <div key={index}>
+            <div>
+              <img src={doc.image} alt="" />
+            </div>
+            <div>
+              <p>{doc.name}</p>
+              <p>{doc.speciality}</p>
+              <p>Address:</p>
+              <p>{doc.address.line1}</p>
+              <p>{doc.address.line2}</p>
+              <p>
+                <span>Date & Time:</span> 25, July, 2025 | 08:30 PM
+              </p>
+            </div>
+            <div></div>
+            <div>
+              <button>Pay Online</button>
+              <button>Cancel appointment</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Appointments;
