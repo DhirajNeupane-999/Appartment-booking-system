30e239ef85632c65a837909459ff3e99a8f27b21
Mahmud-Alam <official.mahmud23@gmail.com>
2025-04-28 19:51:19 +0600

    user profile name onchange

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,7 +1,39 @@
-const Profile = () => {
-  return (
-    <div>Profile</div>
-  )
-}
+import { useState } from "react";
+import { assets } from "../assets/assets_frontend/assets";
 
-export default Profile
+const Profile = () => {
+  const [userData, setUserData] = useState({
+    name: "Md Mahmud Alam",
+    image: assets.profile_pic,
+    email: "mahmudalam.official@gmail.com",
+    phone: "+880 1234-567890",
+    address: {
+      line1: "Mirpur 10",
+      line2: "Dhaka, Bangladesh",
+    },
+    gender: "Male",
+    dob: "2001-10-02",
+    bloodGroup: "O+",
+  });
+
+  const [isEdit, setIsEdit] = useState(false);
+
+  return (
+    <div>
+      <img src={userData.image} alt="" />
+      {isEdit ? (
+        <input
+          onChange={(e) =>
+            setUserData(prev => ({ ...prev, name: e.target.value }))
+          }
+          value={userData.name}
+          type="text"
+        />
+      ) : (
+        <p>{userData.name}</p>
+      )}
+    </div>
+  );
+};
+
+export default Profile;
