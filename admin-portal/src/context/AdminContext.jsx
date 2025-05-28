d45c64aa11e88734926807800d390f24fd134b9b
Mahmud-Alam <official.mahmud23@gmail.com>
2025-05-28 23:50:44 +0600

    set token into admin context

--- admin-portal/src/context/AdminContext.jsx
+++ admin-portal/src/context/AdminContext.jsx
@@ -1,9 +1,18 @@
+import { useState } from "react";
 import { createContext } from "react";
 
 export const AdminContext = createContext();
 
 export const AdminContextProvider = ({ children }) => {
-  const value = {};
+  const [aToken, setAToken] = useState("");
+
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
+  const value = {
+    aToken,
+    setAToken,
+    backendUrl,
+  };
   return (
     <AdminContext.Provider value={value}>{children}</AdminContext.Provider>
   );
