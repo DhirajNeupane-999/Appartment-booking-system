6dac8a17c4c173f96c3aff0801a1db2f9eb26b9d
Mahmud-Alam <official.mahmud23@gmail.com>
2025-06-01 12:24:43 +0600

    get doctors through api

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,10 +1,30 @@
 import { createContext } from "react";
-import { doctors } from "../assets/assets_frontend/assets";
+import { useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { useEffect } from "react";
 
 export const AppContext = createContext();
 
 const AppContextProvider = (props) => {
   const currencySymbol = "$";
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const [doctors, setDoctors] = useState([]);
+
+  const getDoctorsData = async () => {
+    try {
+      const { data } = await axios.get(backendUrl + "/api/doctor/list");
+      if (data.success) {
+        setDoctors(data.doctors);
+      }
+    } catch (err) {
+      toast.error(err.response?.data?.message || "Failed to fetch doctors");
+    }
+  };
+
+  useEffect(() => {
+    getDoctorsData();
+  }, []);
 
   const value = {
     doctors,
