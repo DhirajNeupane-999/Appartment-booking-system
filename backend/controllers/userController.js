4fe81ad2916b219c95267bc9cc61355739826a25
Mahmud-Alam <official.mahmud23@gmail.com>
2025-06-04 00:49:33 +0600

    create user controller and created user registration function

---
+++ backend/controllers/userController.js
@@ -0,0 +1,42 @@
+import validator from "validator";
+import bcrypt from "bcrypt";
+
+// API to register user
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "Please fill in all required fields.",
+      });
+    }
+
+    if (!validator.isEmail(email)) {
+      return res.status(400).json({
+        success: false,
+        message: "Please enter a valid email address.",
+      });
+    }
+
+    if (password.length < 8) {
+      return res.status(400).json({
+        success: false,
+        message: "Password must be at least 8 characters long.",
+      });
+    }
+
+    // hashing user password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const userData = {
+      name,
+      email,
+      password: hashedPassword,
+    };
+
+    
+  } catch (error) {}
+};
