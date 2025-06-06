a3fa7cfbffda04b7094b56cd6b0687b2b6713952
Mahmud-Alam <official.mahmud23@gmail.com>
2025-06-06 20:09:45 +0600

    logout functionality

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import { Link, NavLink, useNavigate } from "react-router-dom";
 import { assets } from "../assets/assets_frontend/assets";
 import { useState } from "react";
+import { useContext } from "react";
+import { AppContext } from "../context/AppContext";
 
 const navLinks = [
   { title: "Home", path: "/" },
@@ -12,9 +14,15 @@ const navLinks = [
 const Navbar = () => {
   const navigate = useNavigate();
 
+  const { token, setToken } = useContext(AppContext);
   const [showMenu, setShowMenu] = useState(false);
-  const [token, setToken] = useState(true);
   const [openDropdown, setOpenDropdown] = useState(false);
+
+  const logoutFunc = () => {
+    setToken(false);
+    localStorage.removeItem("token");
+    navigate("/");
+  };
 
   return (
     <div className="flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400 bg-white sticky top-0 z-50">
@@ -42,7 +50,11 @@ const Navbar = () => {
               alt="user profile pic"
             />
             <img className="w-2.5" src={assets.dropdown_icon} alt="" />
-            <div className={`${openDropdown ? "block" : "hidden"} absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20`}>
+            <div
+              className={`${
+                openDropdown ? "block" : "hidden"
+              } absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20`}
+            >
               <div className="min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4">
                 <p
                   onClick={() => navigate("/profile")}
@@ -57,7 +69,7 @@ const Navbar = () => {
                   My Appointments
                 </p>
                 <p
-                  onClick={() => setToken(false)}
+                  onClick={logoutFunc}
                   className="hover:text-black cursor-pointer"
                 >
                   Logout
