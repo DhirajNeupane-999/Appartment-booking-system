9bb54fcbcded2730bb1a5601cceade6895068eb4
Mahmud-Alam <official.mahmud23@gmail.com>
2025-06-09 17:01:00 +0600

    create get all appointments controller func

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -3,6 +3,7 @@ import bcrypt from "bcrypt";
 import { v2 as cloudinary } from "cloudinary";
 import doctorModel from "../models/doctorModel.js";
 import jwt from "jsonwebtoken";
+import appointmentModel from "../models/appointmentModel.js";
 
 // API for adding doctor
 const addDoctor = async (req, res) => {
@@ -147,4 +148,21 @@ const getAllDoctors = async (req, res) => {
   }
 };
 
-export { addDoctor, loginAdmin, getAllDoctors };
+// API to get all appointments list
+const appointmentsAdmin = async (req, res) => {
+  try {
+    const appointments = await appointmentModel.find({});
+
+    res.status(200).json({
+      success: true,
+      appointments,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export { addDoctor, loginAdmin, getAllDoctors, appointmentsAdmin };
