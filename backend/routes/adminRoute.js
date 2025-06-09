3e40dd1442535108674e3ea7429d0786e01e266f
Mahmud-Alam <official.mahmud23@gmail.com>
2025-06-09 17:01:25 +0600

    created route for get all appointments

--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -1,5 +1,10 @@
 import express from "express";
-import { addDoctor, getAllDoctors, loginAdmin } from "../controllers/adminController.js";
+import {
+  addDoctor,
+  appointmentsAdmin,
+  getAllDoctors,
+  loginAdmin,
+} from "../controllers/adminController.js";
 import upload from "../middlewares/multer.js";
 import authAdmin from "../middlewares/authAdmin.js";
 import { changeAvailability } from "../controllers/doctorController.js";
@@ -7,9 +12,10 @@ import { changeAvailability } from "../controllers/doctorController.js";
 const adminRouter = express.Router();
 
 // Route to add a doctor
-adminRouter.post("/add-doctor", authAdmin , upload.single("image"), addDoctor);
+adminRouter.post("/add-doctor", authAdmin, upload.single("image"), addDoctor);
 adminRouter.post("/login", loginAdmin);
-adminRouter.get("/all-doctors", authAdmin, getAllDoctors)
-adminRouter.post("/change-availability", authAdmin, changeAvailability)
+adminRouter.get("/all-doctors", authAdmin, getAllDoctors);
+adminRouter.post("/change-availability", authAdmin, changeAvailability);
+adminRouter.get("/appointments", authAdmin, appointmentsAdmin);
 
 export default adminRouter;
