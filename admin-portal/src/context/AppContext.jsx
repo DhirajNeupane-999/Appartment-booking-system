896ebaa02ee7c2ac7c44c0d546e160b55c759448
Mahmud-Alam <official.mahmud23@gmail.com>
2025-06-09 21:29:24 +0600

    created calculateAge global func

--- admin-portal/src/context/AppContext.jsx
+++ admin-portal/src/context/AppContext.jsx
@@ -3,7 +3,17 @@ import { createContext } from "react";
 export const AppContext = createContext();
 
 export const AppContextProvider = ({ children }) => {
-  const value = {};
+  const calculateAge = (dob) => {
+    const today = new Date();
+    const birthDate = new Date(dob);
+
+    let age = today.getFullYear() - birthDate.getFullYear();
+
+    return age;
+  };
+  const value = {
+    calculateAge,
+  };
 
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 };
