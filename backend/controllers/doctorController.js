eb50e6971c4fda7233b2265c42649b30edc44fee
Mahmud-Alam <official.mahmud23@gmail.com>
2025-06-11 01:08:39 +0600

    remove login expires for dev

--- backend/controllers/doctorController.js
+++ backend/controllers/doctorController.js
@@ -66,9 +66,11 @@ const loginDoctor = async (req, res) => {
       });
     }
 
-    const token = jwt.sign({ id: doctor._id }, process.env.JWT_SECRET, {
-      expiresIn: "1h",
-    });
+    const token = jwt.sign(
+      { id: doctor._id },
+      process.env.JWT_SECRET
+      // {expiresIn: "1h",}
+    );
 
     res.status(200).json({
       success: true,
