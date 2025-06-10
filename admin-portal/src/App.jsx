7eee565ef4517589ae3609cb1f5f193fbaef27f7
Mahmud-Alam <official.mahmud23@gmail.com>
2025-06-10 12:43:46 +0600

    [CHANGE] created role based path access in App.jsx file updated

--- admin-portal/src/App.jsx
+++ admin-portal/src/App.jsx
@@ -1,35 +1,68 @@
 import { useContext } from "react";
-import Login from "./pages/Login";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/auth/Login";
+import NotAuthorized from "./pages/auth/NotAuthorized";
+import NotFound from "./pages/auth/NotFound";
+
 import { AdminContext } from "./context/AdminContext";
-import Navbar from "./components/Navbar";
-import Sidebar from "./components/Sidebar";
-import { Routes, Route } from "react-router-dom";
-import Dashboard from "./pages/admin/Dashboard";
-import AllAppointments from "./pages/admin/AllAppointments";
-import AddDoctor from "./pages/admin/AddDoctor";
-import DoctorsList from "./pages/admin/DoctorsList";
+import { DoctorContext } from "./context/DoctorContext";
+
+import AdminLayout from "./layouts/AdminLayout";
+import DoctorLayout from "./layouts/DoctorLayout";
 
 const App = () => {
   const { aToken } = useContext(AdminContext);
+  const { dToken } = useContext(DoctorContext);
 
-  return aToken ? (
-    <div className="bg-[#F8F9FD]">
-      <Navbar />
-      <div className="flex items-start">
-        <Sidebar />
-        <Routes>
-          <Route path="/" element={<></>} />
-          <Route path="/admin-dashboard" element={<Dashboard />} />
-          <Route path="/all-appointments" element={<AllAppointments />} />
-          <Route path="/add-doctor" element={<AddDoctor />} />
-          <Route path="/doctors-list" element={<DoctorsList />} />
-        </Routes>
-      </div>
-    </div>
-  ) : (
-    <div>
-      <Login />
-    </div>
+  const isAdmin = !!aToken;
+  const isDoctor = !!dToken;
+
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={
+          isAdmin ? (
+            <Navigate to="/admin/dashboard" />
+          ) : isDoctor ? (
+            <Navigate to="/doctor/dashboard" />
+          ) : (
+            <Login />
+          )
+        }
+      />
+
+      {/* Admin Routes */}
+      <Route
+        path="/admin/*"
+        element={
+          isAdmin ? (
+            <AdminLayout />
+          ) : isDoctor ? (
+            <NotAuthorized />
+          ) : (
+            <Navigate to="/" />
+          )
+        }
+      />
+
+      {/* Doctor Routes */}
+      <Route
+        path="/doctor/*"
+        element={
+          isDoctor ? (
+            <DoctorLayout />
+          ) : isAdmin ? (
+            <NotAuthorized />
+          ) : (
+            <Navigate to="/" />
+          )
+        }
+      />
+
+      {/* Catch-All */}
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 };
 
