bf29e9ebf7c87544ec4415c873d478760dfa2b84
Mahmud-Alam <official.mahmud23@gmail.com>
2025-06-10 12:58:19 +0600

    [UPDATED] sidebar for both admin and doctors

--- admin-portal/src/components/Sidebar.jsx
+++ admin-portal/src/components/Sidebar.jsx
@@ -2,8 +2,9 @@ import { useContext } from "react";
 import { AdminContext } from "../context/AdminContext";
 import { NavLink } from "react-router-dom";
 import { assets } from "../assets/assets";
+import { DoctorContext } from "../context/DoctorContext";
 
-const navLinks = [
+const adminNavLinks = [
   { to: "/admin/dashboard", icon: assets.home_icon, label: "Dashboard" },
   {
     to: "/admin/all-appointments",
@@ -14,18 +15,39 @@ const navLinks = [
   { to: "/admin/doctors-list", icon: assets.home_icon, label: "Doctor List" },
 ];
 
+const doctorNavLinks = [
+  { to: "/doctor/dashboard", icon: assets.home_icon, label: "Dashboard" },
+  {
+    to: "/doctor/appointments",
+    icon: assets.appointment_icon,
+    label: "My Appointments",
+  },
+  { to: "/doctor/profile", icon: assets.people_icon, label: "Profile" },
+];
+
 const Sidebar = () => {
   const { aToken } = useContext(AdminContext);
+  const { dToken } = useContext(DoctorContext);
   return (
     <div className="min-h-screen bg-white border-r">
-      {aToken && (
+      {(aToken || dToken) && (
         <ul className="text-[#515151] mt-5">
-          {navLinks.map(({ to, icon, label }) => (
-            <NavLink className={({isActive})=>`flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary':''}`} key={to} to={to}>
-              <img src={icon} alt={label} />
-              <p>{label}</p>
-            </NavLink>
-          ))}
+          {(aToken ? adminNavLinks : doctorNavLinks).map(
+            ({ to, icon, label }) => (
+              <NavLink
+                className={({ isActive }) =>
+                  `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${
+                    isActive ? "bg-[#F2F3FF] border-r-4 border-primary" : ""
+                  }`
+                }
+                key={to}
+                to={to}
+              >
+                <img src={icon} alt={label} />
+                <p>{label}</p>
+              </NavLink>
+            )
+          )}
         </ul>
       )}
     </div>
