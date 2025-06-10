870df2bd2f5b93b263132150574bae20d67a8297
Mahmud-Alam <official.mahmud23@gmail.com>
2025-06-11 00:22:03 +0600

    created getappoinments func in doc context

--- admin-portal/src/context/DoctorContext.jsx
+++ admin-portal/src/context/DoctorContext.jsx
@@ -1,5 +1,7 @@
+import axios from "axios";
 import { useState } from "react";
 import { createContext } from "react";
+import { toast } from "react-toastify";
 
 export const DoctorContext = createContext();
 
@@ -7,8 +9,31 @@ export const DoctorContextProvider = ({ children }) => {
   const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
   const [dToken, setDToken] = useState(localStorage.getItem("dToken") || "");
+  const [appointments, setAppointments] = useState([]);
 
-  const value = { dToken, setDToken };
+  const getAppointments = async () => {
+    try {
+      const { data } = await axios.get(
+        backendUrl + "/api/doctor/appointments",
+        { headers: { dToken } }
+      );
+
+      if (data.success) {
+        setAppointments(data.appointments.reverse());
+      }
+    } catch (error) {
+      toast.error(
+        error.response?.data?.message || "Failed to fetch appointments"
+      );
+    }
+  };
+
+  const value = {
+    dToken,
+    setDToken,
+    appointments,
+    getAppointments,
+  };
   return (
     <DoctorContext.Provider value={value}>{children}</DoctorContext.Provider>
   );
