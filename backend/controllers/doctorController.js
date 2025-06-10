bf55d08223a58b5ea38376203713591b48a318cb
Mahmud-Alam <official.mahmud23@gmail.com>
2025-06-10 09:15:40 +0600

    created doctor login controller and route

--- backend/controllers/doctorController.js
+++ backend/controllers/doctorController.js
@@ -1,4 +1,6 @@
 import doctorModel from "../models/doctorModel.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const changeAvailability = async (req, res) => {
   try {
@@ -34,4 +36,49 @@ const doctorList = async (req, res) => {
   }
 };
 
-export { changeAvailability, doctorList };
+// API for doctor login
+const loginDoctor = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      res.status(400).json({
+        success: false,
+        message: "Required fields are empty.",
+      });
+    }
+
+    const doctor = await doctorModel.findOne({ email });
+
+    if (!doctor) {
+      res.status(404).json({
+        success: false,
+        message: "Invalid email",
+      });
+    }
+
+    const isMatch = await bcrypt.compare(password, doctor.password);
+
+    if (!isMatch) {
+      res.status(401).json({
+        success: false,
+        message: "Password is incorrect",
+      });
+    }
+
+    const token = jwt.sign({ id: doctor._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+
+    res.status(200).json({
+      success: true,
+      token,
+    });
+  } catch (error) {
+    res.status(401).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export { changeAvailability, doctorList, loginDoctor };
