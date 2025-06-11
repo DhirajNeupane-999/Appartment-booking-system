3456aaf0a90ae19f0b478b8f2e8c4b35935ce7b3
Mahmud-Alam <official.mahmud23@gmail.com>
2025-06-11 17:57:49 +0600

    checked iscompleted flag

--- admin-portal/src/pages/admin/AllAppointments.jsx
+++ admin-portal/src/pages/admin/AllAppointments.jsx
@@ -4,7 +4,8 @@ import { AppContext } from "../../context/AppContext";
 import { assets } from "../../assets/assets";
 
 const AllAppointments = () => {
-  const { aToken, appointments, getAllAppointments, cancelAppointment } = useContext(AdminContext);
+  const { aToken, appointments, getAllAppointments, cancelAppointment } =
+    useContext(AdminContext);
   const { currencySymbol, calculateAge, formatDateString } =
     useContext(AppContext);
 
@@ -27,7 +28,7 @@ const AllAppointments = () => {
           <p>Payment</p>
           <p>Actions</p>
         </div>
-        {appointments.map((item, index) => (
+        {[...appointments].reverse().map((item, index) => (
           <div
             key={index}
             className="flex flex-wrap justify-between max-sm:gap-2 sm:grid sm:grid-cols-[0.5fr_3fr_1fr_3fr_3fr_1fr_1fr_1fr] items-center text-gray-500 py-3 px-6 border-b border-gray-400 hover:bg-gray-50"
@@ -58,17 +59,22 @@ const AllAppointments = () => {
               {item.docData.fees}
             </p>
             <p
-              className={`text-xs font-medium ${
-                item.payment ? "text-green-400" : "text-red-400"
+              className={`text-xs w-fit border px-2 rounded-full ${
+                item.payment
+                  ? "bg-green-400 text-white border-green-600"
+                  : "text-primary border-primary"
               }`}
             >
-              {item.payment ? "Paid" : "N/A"}
+              {item.payment ? "ONLINE" : "CASH"}
             </p>
+
             {item.cancelled ? (
               <p className="text-red-400 text-xs font-medium">Cancelled</p>
+            ) : item.isCompleted ? (
+              <p className="text-green-500 text-xs font-medium">Completed</p>
             ) : (
               <img
-              onClick={()=>cancelAppointment(item._id)}
+                onClick={() => cancelAppointment(item._id)}
                 className="w-10 cursor-pointer"
                 src={assets.cancel_icon}
                 alt=""
