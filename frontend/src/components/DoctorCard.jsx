048748c802dab6343f93666fc1159794e3a84484
Mahmud-Alam <official.mahmud23@gmail.com>
2025-06-11 20:06:45 +0600

    doctor available status and update ui change

--- frontend/src/components/DoctorCard.jsx
+++ frontend/src/components/DoctorCard.jsx
@@ -6,14 +6,25 @@ const DoctorCard = ({ doctor }) => {
 
   return (
     <div
-      onClick={() => {navigate(`/appointment/${doctor._id}`); scrollTo(0,0)}}
-      className="border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500"
+      onClick={() => {
+        navigate(`/appointment/${doctor._id}`);
+        scrollTo(0, 0);
+      }}
+      className={`border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500 ${
+        doctor.available ? "" : "opacity-50"
+      }`}
     >
       <img className="bg-blue-50" src={doctor.image} alt={doctor.name} />
       <div className="p-4">
-        <div className="flex items-center gap-2 text-sm text-center text-green-500">
-          <p className="w-2 h-2 bg-green-500 rounded-full"></p>
-          <p>Available</p>
+        <div className="flex items-center gap-2 text-sm text-center">
+          <p
+            className={`w-2 h-2 rounded-full ${
+              doctor.available ? "bg-green-500" : "bg-gray-400"
+            }`}
+          ></p>
+          <p className={doctor.available ? "text-green-500" : "text-gray-400"}>
+            {doctor.available ? "Available" : "Not Available"}
+          </p>
         </div>
         <p className="text-gray-900 text-lg font-medium">{doctor.name}</p>
         <p className="text-gray-600 text-sm">{doctor.speciality}</p>
--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -129,11 +129,22 @@ const Appointment = () => {
     getAvailableSlots();
   }, [docInfo]);
 
+  useEffect(() => {
+    if (docInfo && !docInfo.available) {
+      toast.info(
+        `${docInfo.name} is currently unavailable. You may book an appointment with one of our other top-rated doctors.`,{
+          autoClose: 10000,
+          draggable: true
+        }
+      );
+    }
+  }, [docInfo]);
+
   return (
     docInfo && (
       <div>
         <div className="flex flex-col sm:flex-row gap-4">
-          <div>
+          <div className={docInfo.available ? "" : "opacity-50"}>
             <img
               className="bg-primary w-full sm:max-w-72 rounded-lg"
               src={docInfo.image}
@@ -174,49 +185,59 @@ const Appointment = () => {
         </div>
 
         {/* ---------- Booking slots ---------- */}
-        <div className="sm:ml-72 sm:pl-4 mt-4 font-medium text-gray-700">
-          <p>Booking slots</p>
-          <div className="flex gap-3 items-center w-full overflow-x-scroll mt-4">
-            {docSlots.length &&
-              docSlots.map((item, index) => (
-                <div
-                  onClick={() => setSlotIndex(index)}
-                  className={`text-center py-6 min-w-16 rounded-full cursor-pointer ${
-                    slotIndex === index
-                      ? "bg-primary text-white"
-                      : "border border-gray-200"
-                  }`}
-                  key={index}
-                >
-                  <p>{item[0] && daysOfWeek[item[0].datetime.getDay()]}</p>
-                  <p>{item[0] && item[0].datetime.getDate()}</p>
-                </div>
-              ))}
-          </div>
+        {docInfo.available ? (
+          <div className="sm:ml-72 sm:pl-4 mt-4 font-medium text-gray-700">
+            <p>Booking slots</p>
+            <div className="flex gap-3 items-center w-full overflow-x-scroll mt-4">
+              {docSlots.length &&
+                docSlots.map((item, index) => (
+                  <div
+                    onClick={() => setSlotIndex(index)}
+                    className={`text-center py-6 min-w-16 rounded-full cursor-pointer ${
+                      slotIndex === index
+                        ? "bg-primary text-white"
+                        : "border border-gray-200"
+                    }`}
+                    key={index}
+                  >
+                    <p>{item[0] && daysOfWeek[item[0].datetime.getDay()]}</p>
+                    <p>{item[0] && item[0].datetime.getDate()}</p>
+                  </div>
+                ))}
+            </div>
 
-          <div className="flex items-center gap-3 w-full overflow-x-scroll mt-4">
-            {docSlots.length &&
-              docSlots[slotIndex].map((item, index) => (
-                <p
-                  onClick={() => setSlotTime(item.time)}
-                  className={`text-sm font-light flex-shrink-0 px-5 py-2 rounded-full cursor-pointer ${
-                    item.time === slotTime
-                      ? "bg-primary text-white"
-                      : "text-gray-400 border border-gray-300"
-                  }`}
-                  key={index}
-                >
-                  {item.time.toLowerCase()}
-                </p>
-              ))}
+            <div className="flex items-center gap-3 w-full overflow-x-scroll mt-4">
+              {docSlots.length &&
+                docSlots[slotIndex].map((item, index) => (
+                  <p
+                    onClick={() => setSlotTime(item.time)}
+                    className={`text-sm font-light flex-shrink-0 px-5 py-2 rounded-full cursor-pointer ${
+                      item.time === slotTime
+                        ? "bg-primary text-white"
+                        : "text-gray-400 border border-gray-300"
+                    }`}
+                    key={index}
+                  >
+                    {item.time.toLowerCase()}
+                  </p>
+                ))}
+            </div>
+            <button
+              onClick={bookAppointment}
+              className="bg-primary text-white text-sm font-light px-14 py-3 rounded-full my-6 cursor-pointer"
+            >
+              Book an appointment
+            </button>
           </div>
-          <button
-            onClick={bookAppointment}
-            className="bg-primary text-white text-sm font-light px-14 py-3 rounded-full my-6 cursor-pointer"
-          >
-            Book an appointment
-          </button>
-        </div>
+        ) : (
+          <div className="mt-10 flex items-center gap-2 text-sm text-center">
+            <p className="w-2 h-2 rounded-full bg-gray-500"></p>
+            <p className="text-gray-500 font-semibold">
+              {docInfo.name} is currently unavailable. You may book an
+              appointment with one of our other top-rated doctors.
+            </p>
+          </div>
+        )}
 
         {/* Related Doctors section */}
         <RelatedDoctors docId={docId} speciality={docInfo.speciality} />
