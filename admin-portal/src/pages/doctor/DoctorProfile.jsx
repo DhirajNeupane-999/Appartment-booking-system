7e10743f96345712e159da04d77954c1b92f4f5a
Mahmud-Alam <official.mahmud23@gmail.com>
2025-06-11 14:22:18 +0600

    created doctor profile full functional

--- admin-portal/src/pages/doctor/DoctorProfile.jsx
+++ admin-portal/src/pages/doctor/DoctorProfile.jsx
@@ -1,11 +1,41 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { DoctorContext } from "../../context/DoctorContext";
 import { AppContext } from "../../context/AppContext";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const DoctorProfile = () => {
   const { dToken, profileData, setProfileData, getProfileData } =
     useContext(DoctorContext);
   const { currencySymbol, backendUrl } = useContext(AppContext);
+
+  const [isEdit, setIsEdit] = useState(false);
+
+  const updateDoctorProfile = async () => {
+    try {
+      const updateData = {
+        address: profileData.address,
+        fees: profileData.fees,
+        available: profileData.available,
+      };
+
+      const { data } = await axios.post(
+        backendUrl + "/api/doctor/update-profile",
+        updateData,
+        { headers: { dToken } }
+      );
+
+      if (data.success) {
+        toast.success(data.message);
+        setIsEdit(false);
+        getProfileData();
+      }
+    } catch (error) {
+      toast.error(
+        error.response?.data?.message || "Failed to cancel appointments"
+      );
+    }
+  };
 
   useEffect(() => {
     if (dToken) {
@@ -52,27 +82,95 @@ const DoctorProfile = () => {
             <p className="text-gray-600 font-medium mt-4">
               Appointment fee:{" "}
               <span className="text-gray-800">
-                {currencySymbol} {profileData.fees}
+                {currencySymbol}{" "}
+                {isEdit ? (
+                  <input
+                    className="border rounded pl-2"
+                    type="number"
+                    onChange={(e) =>
+                      setProfileData((prev) => ({
+                        ...prev,
+                        fees: e.target.value,
+                      }))
+                    }
+                    value={profileData.fees}
+                  />
+                ) : (
+                  profileData.fees
+                )}
               </span>
             </p>
 
             <div className="flex gap-2 py-2">
               <p>Address:</p>
               <p className="text-sm">
-                {profileData.address.line1}
+                {isEdit ? (
+                  <input
+                    className="border rounded pl-2 mb-1"
+                    type="text"
+                    onChange={(e) =>
+                      setProfileData((prev) => ({
+                        ...prev,
+                        address: { ...prev.address, line1: e.target.value },
+                      }))
+                    }
+                    value={profileData.address.line1}
+                  />
+                ) : (
+                  profileData.address.line1
+                )}
                 <br />
-                {profileData.address.line2}
+                {isEdit ? (
+                  <input
+                    className="border rounded pl-2"
+                    type="text"
+                    onChange={(e) =>
+                      setProfileData((prev) => ({
+                        ...prev,
+                        address: { ...prev.address, line2: e.target.value },
+                      }))
+                    }
+                    value={profileData.address.line2}
+                  />
+                ) : (
+                  profileData.address.line2
+                )}
               </p>
             </div>
 
             <div className="flex gap-1 pt-2">
-              <input className="cursor-pointer" type="checkbox" name="" id="" />
+              <input
+                onChange={(e) =>
+                  isEdit &&
+                  setProfileData((prev) => ({
+                    ...prev,
+                    available: !prev.available,
+                  }))
+                }
+                checked={profileData.available}
+                className="cursor-pointer"
+                type="checkbox"
+                name=""
+                id=""
+              />
               <label htmlFor="">Available</label>
             </div>
 
-            <button className="px-4 py-1 border border-primary text-sm rounded-full mt-5 text-primary cursor-pointer hover:bg-primary hover:text-white transition-all duration-300">
-              Edit
-            </button>
+            {isEdit ? (
+              <button
+                onClick={updateDoctorProfile}
+                className="px-4 py-1 border border-primary text-sm rounded-full mt-5 text-primary cursor-pointer hover:bg-primary hover:text-white transition-all duration-300"
+              >
+                Save
+              </button>
+            ) : (
+              <button
+                onClick={() => setIsEdit(true)}
+                className="px-4 py-1 border border-primary text-sm rounded-full mt-5 text-primary cursor-pointer hover:bg-primary hover:text-white transition-all duration-300"
+              >
+                Edit
+              </button>
+            )}
           </div>
         </div>
       </div>
